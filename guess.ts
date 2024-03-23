5d9a0432d49731de680ed609f9251202d9e4d74d
AYeshsidd <159470548+AYeshsidd@users.noreply.github.com>
2024-03-23 16:18:36 -0700

    console

--- guess.ts
+++ guess.ts
@@ -4,11 +4,17 @@ import inquirer from "inquirer";
 
 const randomnumber = Math.floor(Math.random() * 6 + 1);
 
+console.log("guess for 100$");
+
+
+
 const answer = await inquirer.prompt([{
     
     name: "userguessnumber",
     type:"number",
     message:"please guess the number between (1 - 6)"
+    
+    
 
 }])
 
